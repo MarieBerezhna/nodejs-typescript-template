bad9adc6a65870d9181f6011dd8199231ad5f51f
Marie Berezhna <marie.berezhna@gmail.com>
2024-12-03 19:04:25 +0200

    cleanup

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,8 +1,5 @@
 import { Router } from 'express';
-import upload from '../utils/upload';
 import fs from 'fs';
-import convert from '../utils/convert';
-import { createUserSpace } from '../utils/generic';
 
 const mainRoute = Router();
 
@@ -27,44 +24,6 @@ mainRoute.get('/', (req:any, res) => {
   console.log(req.sessionID);
     res.send('main api route works');
 });
-
-mainRoute.post('/upload', upload.array('files'), (req:any, res) => {
-
-  createUserSpace(req.sessionID);
-
-  console.log(req.body, req.file, req.files, req.data);
-  
-  // Handle the uploaded file
-  res.json({ message: 'File uploaded successfully!' });
-});
-
-mainRoute.get('/convert', (req:any, res) => {
-  const userDir = `src/users/${req.sessionID}`;
-  const uploadsDir = `${userDir}/uploads/`;
-  const outputDir = `${userDir}/output/`;
-
-  if (!fs.existsSync(uploadsDir))  res.json({ message: 'convert! - empty' });
-  if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir);
-
-  fs.readdir(uploadsDir, (err: any, files: any[]) => { 
-    if (err) 
-      console.log(err); 
-    else { 
-
-      files.forEach(file => { 
-        const ext = file.split('.').pop();
-        const filename = file.replace(`.${ext}`, '');
-        const inputPath = `${uploadsDir}${file}`;
-        const outputPath = `${outputDir}${filename}.webp`;
-
-        convert(inputPath, outputPath);
-      }) 
-    } 
-  });
-
-    res.json({ message: 'convert!' });
-});
-
 
 
 
